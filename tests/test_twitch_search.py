447e208f130f92484a564cf3490e165e7e48217b
gerben <okiilaska@gmail.com>
2026-05-17 15:32:33 +0800

    Extract variable names to config file

--- tests/test_twitch_search.py
+++ tests/test_twitch_search.py
@@ -12,16 +12,15 @@ from utils.config_reader import (
 def test_twitch_streamer_flow(driver):
 
     config = load_config()
-    base_url = config["base_url"]
     home_page = HomePage(driver)
     search_page = SearchPage(driver)
     streamer_page = StreamerPage(driver)
 
-    home_page.open(base_url)
+    home_page.open(config["base_url"])
     streamer_page.handle_popup()
     home_page.click_search()
 
-    search_page.search_game("StarCraft II")
+    search_page.search_game(config["search_name"])
     search_page.scroll_twice(2)
     search_page.select_first_streamer()
 

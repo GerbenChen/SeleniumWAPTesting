76bbca3dfc9ed3859a0cfa968af16bd21d31341f
gerben <okiilaska@gmail.com>
2026-05-17 14:28:39 +0800

    fix github action fail

--- tests/conftest.py
+++ tests/conftest.py
@@ -45,6 +45,8 @@ def driver(request):
         "useAutomationExtension",
         False
     )
+    chrome_options.add_argument("--no-sandbox") 
+    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--window-size=390,844")
     chrome_options.add_argument("--disable-popup-blocking")
     chrome_options.add_argument("--disable-notifications")
